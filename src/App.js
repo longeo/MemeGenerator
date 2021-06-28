2a0ce88f1946263a41d1db25defcbd40a4f433e0
longeo <longeo@tcd.ie>
2021-06-28 20:32:44 +0100

    set up boilerplate project code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component{
+    render(){
+        return( <h1> Hello, World!</h1>)
+    }
 }
 
-export default App;
+export default App
