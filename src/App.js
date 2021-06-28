d62cde24d8233a9ff5f4ae71c17805f3b8a9f04a
longeo <longeo@tcd.ie>
2021-06-28 23:22:02 +0100

    Fetching data from meme API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react"
 
-class App extends React.Component{
-    render(){
-        return( <h1> Hello, World!</h1>)
-    }
+import Header from "./Header"
+import MemeGenerator from "./MemeGenerator"
+/**
+ * Create 2 new components - Header and MemeGenerator
+ * Header will only display things
+ * MemeGenerator will be calling to an API and holding on to data
+ * Each should be in their own file of the same name
+ */
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <MemeGenerator />
+        </div>
+    )
 }
 
 export default App
---
+++ src/MemeGenerator.js
@@ -0,0 +1,42 @@
+import React, {Component} from "react"
+
+class MemeGenerator extends Component {
+    constructor() {
+        super()
+        this.state = {
+            topText: "",
+            bottomText: "",
+            randomImg: "http://i.imgflip.com/1bij.jpg",
+            allMemeImgs: ""
+        }
+    }  
+    /**
+     * We'll be using an API that provides a bunch of meme images.
+     * 
+     * Your task:
+     * make an API call to "https://api.imgflip.com/get_memes" and save the 
+     * data that comes back (`response.data.memes`) to a new state property
+     * called `allMemeImgs`. (The data that comes back is an array)
+     */
+    
+    componentDidMount(){
+        fetch("https://api.imgflip.com/get_memes")
+        .then(response => response.json())
+        .then(response => {
+            const {memes} = response.data
+            console.log([memes[0]])
+            this.setState({
+                allMemeImgs: memes
+            })
+            }
+            )
+        }
+    
+    render() {
+        return (
+            <h1>MEME GENERATOR SECTION</h1>
+        )
+    }
+}
+
+export default MemeGenerator
